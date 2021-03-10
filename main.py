980a33ec1b4e533a17a29afa1595000cc5c1bddf
markserver <markserver@outlook.com>
2021-03-10 17:16:28 +0100

    Grammar

--- main.py
+++ main.py
@@ -1,35 +1,25 @@
-from lark import Lark, Transformer, v_args
+from lark import Lark, Transformer
 
-calc_grammar = """
-%import grammar
-"""
+class MyTransformer(Transformer):
+    def number(self, tok):
+        return float(tok[0].value)
+    def list(self, items):
+        return list(items)
+    def factor(self, items):
+        return items[0]
+    def term(self, items):
+        return items[0]
+    def expression(self, items):
+        return items[0]
+    def sum(self, items):
+        return items[0]+items[1]
+    def pair(self, key_value):
+        k, v = key_value
+        return k, v
+    def dict(self, items):
+        return dict(items)
 
-@v_args(inline=True)    # Affects the signatures of the methods
-class CalculateTree(Transformer):
-    from operator import add, sub, mul, truediv as div, neg
-    number = float
-
-    def __init__(self):
-        self.vars = {}
-
-    def assign_var(self, name, value):
-        self.vars[name] = value
-        return value
-
-    def range(self, name, value, no):
-        print(name)
-        print(value)
-        print(no)
-        print("range test")
-
-    def var(self, name):
-        try:
-            return self.vars[name]
-        except KeyError:
-            raise Exception("Variable not found: %s" % name)
-
-
-calc_parser = Lark.open(calc_grammar, parser='lalr', transformer=CalculateTree())
+calc_parser = Lark.open("grammar.lark", parser='lalr', start='statement')
 calc = calc_parser.parse
 
 
@@ -39,7 +29,11 @@ def main():
             s = input('> ')
         except EOFError:
             break
-        print(calc(s))
+        tree = calc(s)
+        print("Parse Tree:")
+        print(tree.pretty())
+        print("Evaluation:")
+        print(MyTransformer().transform(tree))
 
 
 if __name__ == '__main__':
