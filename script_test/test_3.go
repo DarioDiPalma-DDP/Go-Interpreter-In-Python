f75282d5ec42afea7ebafee69b9f6909f9be0c2c
DarioDiPalma98 <dario.dipalma@outlook.it>
2021-03-20 18:41:50 +0100

    Minor update

--- script_test/test_3.go
+++ script_test/test_3.go
@@ -13,14 +13,3 @@ for i > 1 {
 }
 Printf("No more bottles of beer on the wall, no more bottles of beer.")
 Printf("Go to the store and buy some more, 99 bottles of beer on the wall...")
-
-choice := Scanf("Inserire opzione desiderata (1-Saluto 2-Chi sono 3-Esci): ")
-if choice == "1" {
-	Printf("Ciao!")
-} else if choice == "2" {
-	Printf("Sono un interprete di Go realizzato da Dario e Marco.")
-} else if choice == "3" {
-	Printf("Grazie di avermi utilizzato.")
-} else {
-	Printf("Selezione non corretta.")
-}
--- script_test/test_5.go
+++ script_test/test_5.go
@@ -1,6 +1,6 @@
 x := 2 + b
 var c = 10
-var b =
+var b 
 print("Inizio"))	
 if (9) { print("sono uguale a 9"); } else if ( { print("c è uguale a 12"); } else {
 print("");
